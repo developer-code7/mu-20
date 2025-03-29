2b4d40e4cdf0a5f8fd984f385e11cd5c4a6b2942
Kapil Jain <kapiljain46245@gmail.com>
2025-03-29 17:51:18 +0530

    added testing url

--- src/helper/axiosInstance.ts
+++ src/helper/axiosInstance.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 const axiosInstance = axios.create();
 
-const BASE_URL = import.meta.env.VITE_LOCAL_BASE_URL;
+const BASE_URL = import.meta.env.VITE_RENDER_URL || import.meta.env.VITE_LOCAL_BASE_URL;
 axiosInstance.defaults.baseURL = BASE_URL;
 axiosInstance.defaults.withCredentials = true;
 
